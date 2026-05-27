476a1291d62d3a2cc876bb1ff4286af174f2c8a1
YaghoCodes <yagho.chinaglia@sptech.school>
2026-05-27 16:54:53 -0300

    feat: Kpis funcionando na dashboard, e removi alguns arquivos inuteis

---
+++ src/models/kpiModel.js
@@ -0,0 +1,37 @@
+var database = require("../database/config")
+
+async function buscarRotatividade(empresa){
+console.log("Cheguei no model de buscar a kpi de: rotatividade");
+    var instrucaoSql = `
+        SELECT rotaçao_mesa as rotatividade from
+        vw_rotacao_mesa WHERE idRestaurante = ${empresa}
+                `;
+    console.log("Executando a instrução SQL pra buscar a kpi de: rotatividade: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+async function buscarFluxo(empresa){
+console.log("Cheguei no model de buscar a kpi de: fluxo ");
+    var instrucaoSql = `
+        SELECT fluxo_atual_7dias as atual, fluxo_ideal_7dias as ideal from
+        vw_fluxo_7dias WHERE idRestaurante = ${empresa}
+                `;
+    console.log("Executando a instrução SQL pra buscar a kpi de: fluxo : \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+async function buscarOcupacao(empresa){
+console.log("Cheguei no model de buscar a kpi de: ocupacao ");
+    var instrucaoSql = `
+        SELECT percentual_ocupacao as ocupacao from
+        vw_ocupacao_restaurante WHERE idRestaurante = ${empresa}
+                `;
+    console.log("Executando a instrução SQL pra buscar a kpi de: ocupacao : \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+module.exports = {
+    buscarRotatividade,
+    buscarFluxo,
+    buscarOcupacao
+};
