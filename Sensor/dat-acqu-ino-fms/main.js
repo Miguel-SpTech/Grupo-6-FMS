b2151bf6dd84bb00baa1eadf4898bb88f0225dc6
LuizSptech <luiz.correia@sptech.school>
2026-06-08 10:15:18 -0300

    conectando

--- Sensor/dat-acqu-ino-fms/main.js
+++ Sensor/dat-acqu-ino-fms/main.js
@@ -19,9 +19,9 @@ const serial = async (
     // conexão com o banco de dados MySQL
     let poolBancoDados = mysql.createPool(
         {
-            host: '10.18.12.64',
-            user: 'usuario_insert',
-            password: 'Rml_1505',
+            host: '10.18.32.79',
+            user: 'aluno',
+            password: 'Sptech#2024',
             database: 'FMS',
             port: 3307
         }
